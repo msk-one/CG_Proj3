3bebcb667132bb626d61b71a7b8a855b66f01f42
Michal Szklarski <msk@nex10.pl>
2016-05-02 21:25:22 +0200

    Drawing progress, init algorithms

---
+++ CG_Proj3/CG_Proj3/Algorithms.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CG_Proj3
+{
+    class Algorithms
+    {
+        private void AddPixel(Canvas c, double x, double y)
+        {
+            Rectangle rec = new Rectangle();
+            Canvas.SetTop(rec, y);
+            Canvas.SetLeft(rec, x);
+            rec.Width = 1;
+            rec.Height = 1;
+            rec.Fill = new SolidColorBrush(Colors.Black);
+            c.Children.Add(rec);
+        }
+
+        public void MidpointLine(Canvas c, int x1, int y1, int x2, int y2)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            int d = 2*dy - dx;
+            int dE = 2*dy;
+            int dNE = 2*(dy - dx);
+            int x = x1, y = y1;
+
+            AddPixel(c, x, y);
+            while (x < x2)
+            {
+                if (d < 0)
+                {
+                    d += dE;
+                    x++;
+                }
+                else
+                {
+                    d += dNE;
+                    ++x;
+                    ++y;
+                }
+                AddPixel(c, x, y);
+            }
+        }
+
+        public void SymmetricMidpointLine(Canvas c, int x1, int y1, int x2, int y2)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+
+            int d = 2*dy - dx;
+            int dE = 2*dy;
+            int dNE = 2*(dy - dx);
+            int xf = x1, yf = y1;
+            int xb = x2, yb = y2;
+
+            AddPixel(c, xf, yf);
+            AddPixel(c, xb, yb);
+
+            while (xf < xb)
+            {
+                ++xf;
+                --xb;
+                if (d < 0)
+                    d += dE;
+                else
+                {
+                    d += dNE;
+                    ++yf;
+                    --yb;
+                }
+
+                AddPixel(c, xf, yf);
+                AddPixel(c, xb, yb);
+            }
+        }
+
+        public void MidpointCircle(Canvas c, int R)
+        {
+            int dE = 3;
+            int dSE = 5 - 2*R;
+            int d = 1 - R;
+            int x = 0;
+            int y = R;
+
+            AddPixel(c, x, y);
+            while (y > x)
+            {
+                if (d < 0)
+                {
+                    d += dE;
+                    dE += 2;
+                    dSE += 2;
+                }
+                else
+                {
+                    d += dSE;
+                    dE += 2;
+                    dSE += 4;
+                    --y;
+                }
+                ++x;
+                AddPixel(c, x, y);
+            }
+
+        }
+
+        //int IntensifyPixel(Canvas c, int x, int y, float thickness, float distance)
+        //{
+        //    float cov = coverage(thickness, distance);
+        //    if (cov > 0)
+        //        AddPixel(c, x, y, lerp(BKG_COLOR, LINE_COLOR, cov));
+        //    return cov;
+        //}
+
+        //public int GuptaSproull(int x1, int y1, int x2, int y2, float thickness)
+        //{
+        //    int dx = x2 - x1, dy = y2 - y1;
+        //    int dE = 2*dy, dNE = 2*(dy - dx);
+        //    int d = 2*dy - dx;
+        //    int two_v_dx = 0; //numerator, v=0 for the first pixel
+        //    float invDenom = (float) (1/(2*Math.Sqrt(dx*dx + dy*dy)));
+        //    float two_dx_invDenom = 2*dx*invDenom; //precomputed constant
+        //    int x = x1, y = y1;
+        //    int i;
+        //    IntensifyPixel(x, y, thickness, 0);
+        //    for (i = 1; IntensifyPixel(x, y + i, thickness, i*two_dx_invDenom); ++i) ;
+        //    for (i = 1; IntensifyPixel(x, y - i, thickness, i*two_dx_invDenom); ++i) ;
+        //    while (x < x2)
+        //    {
+        //        ++x;
+        //        if (d < 0) // move to E
+        //        {
+        //            two_v_dx = d + dx;
+        //            d += dE;
+        //        }
+        //        else // move to NE
+        //        {
+        //            two_v_dx = d - dx;
+        //            d += dNE;
+        //            ++y;
+        //        }
+
+        //        IntensifyPixel(x, y, thickness, two_v_dx*invDenom);
+        //        for (i = 1; IntensifyPixel(x, y + i, thickness, i*two_dx_invDenom - two_v_dx*invDenom); ++i);
+        //        for (i = 1; IntensifyPixel(x, y - i, thickness, i*two_dx_invDenom + two_v_dx*invDenom); ++i);
+        //    }
+        //}
+    }
+}
--- CG_Proj3/CG_Proj3/MainWindow.xaml.cs
+++ CG_Proj3/CG_Proj3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,9 +19,15 @@ namespace CG_Proj3
     public partial class MainWindow : Window
     {
         private bool isMouseDown = false;
+        private bool lineMode= false;
+        private bool circleMode = false;
+        private int lineSize = 1;
+        private List<Point> linePoints;
+
         public MainWindow()
         {
             InitializeComponent();
+            linePoints = new List<Point>();
         }
 
         private void mainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
@@ -31,25 +38,82 @@ namespace CG_Proj3
         private void mainCanvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
             isMouseDown = false;
+            if (lineMode)
+            {
+                if (linePoints.Count < 1)
+                {
+                    linePoints.Add(new Point(e.GetPosition(mainCanvas).X, e.GetPosition(mainCanvas).Y));
+                }
+                else if (linePoints.Count == 1)
+                {
+                    linePoints.Add(new Point(e.GetPosition(mainCanvas).X, e.GetPosition(mainCanvas).Y));
+                    //Line line = new Line();
+                    //line.Stroke = Brushes.Black;
+                    //line.StrokeThickness = lineSize;
+                    //line.X1 = linePoints[0].X;
+                    //line.Y1 = linePoints[0].Y;
+                    //line.X2 = linePoints[1].X;
+                    //line.Y2 = linePoints[1].Y;
+
+                    //mainCanvas.Children.Add(line);
+                    //linePoints.Clear();
+
+                    Algorithms alg = new Algorithms();
+                    //TODO: midline, symm. midline, gupta-sproull
+                    if (antialiasing_type.Text == "MidLine")
+                    {
+                        alg.MidpointLine(mainCanvas, Convert.ToInt32(linePoints[0].X), Convert.ToInt32(linePoints[0].Y), Convert.ToInt32(linePoints[1].X), Convert.ToInt32(linePoints[1].Y));
+                    }
+                    else if (antialiasing_type.Text == "Symmetric MidLine")
+                    {
+                        alg.SymmetricMidpointLine(mainCanvas, Convert.ToInt32(linePoints[0].X), Convert.ToInt32(linePoints[0].Y), Convert.ToInt32(linePoints[1].X), Convert.ToInt32(linePoints[1].Y));
+                    }
+                    else if (antialiasing_type.Text == "Gupta-Sproull")
+                    {
+
+                    }
+                    linePoints.Clear();
+                }
+            }
+            else
+            {
+                //Ellipse customCircle = new Ellipse();
+                //Point brushPosition = new Point(e.GetPosition(mainCanvas).X, e.GetPosition(mainCanvas).Y);
+                //customCircle.Fill = Brushes.Transparent;
+                //customCircle.StrokeThickness = lineSize;
+                //customCircle.Stroke = Brushes.Black;
+
+                //customCircle.Width = lineSize*10;
+                //customCircle.Height = lineSize*10;
+
+                //Canvas.SetTop(customCircle, brushPosition.Y);
+                //Canvas.SetLeft(customCircle, brushPosition.X);
+
+                //mainCanvas.Children.Add(customCircle);
+
+                //TODO: midline
+                Algorithms alg = new Algorithms();
+                if (antialiasing_type.Text == "MidLine")
+                {
+                    alg.MidpointCircle(mainCanvas, lineSize*10);
+                }
+            }
         }
 
-        private void mainCanvas_MouseMove(object sender, MouseEventArgs e)
+        private void mainCanvas_MouseMove(object sender, MouseEventArgs e) {}
+
+        private void line_button_Click(object sender, RoutedEventArgs e)
         {
-            Ellipse mybrush = new Ellipse();
-
-            mybrush.Width = 5;
-            mybrush.Height = 5;
-
-            if (isMouseDown)
-            {
-                Point brushPosition = new Point(e.GetPosition(mainCanvas).X, e.GetPosition(mainCanvas).Y);
-                mybrush.Fill = new SolidColorBrush(Colors.Black);
-
-                Canvas.SetTop(mybrush, brushPosition.Y);
-                Canvas.SetLeft(mybrush, brushPosition.X);
-
-                mainCanvas.Children.Add(mybrush);
-            }
+            lineMode = true;
+            circleMode = false;
+            lineSize = Convert.ToInt32(brushSize.Text);
+        }
+   
+        private void circle_button_Click(object sender, RoutedEventArgs e)
+        {
+            lineMode = false;
+            circleMode = true;
+            lineSize = Convert.ToInt32(brushSize.Text);
         }
     }
 }
